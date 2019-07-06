0e82f4871b221844a01de0a411d021dafa63ce3e
Ambrose Chua <ambrose@chua.family>
2019-07-06 23:12:29 +0000

    Read backend2 from env

--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -66,13 +66,14 @@ export default {
   }),
   created: function() {
     // Initialize all the models and format them follow the data format above
-    fetch('http://localhost:10200/models').then((res) => {
+	  const base = process.env.NUXT_ENV_BACKEND2_URL || 'http://localhost:10200';
+    fetch(base + '/models').then((res) => {
       return res.json();
     }).then((body) => {
       return Promise.all(body.models
                         .filter(modelName => modelName != 'parser')
                         .map((modelName) => {
-                          return fetch(`http://localhost:10201/params/${modelName}`)
+							return fetch(`${base}/params/${modelName}`)
                             .then((res) => res.text())
                             .then((loss) => { title: modelName, loss });
                         }));
