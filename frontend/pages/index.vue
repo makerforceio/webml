e930f8145f558c79452fcf26f60c24ba9660ab8a
Ambrose Chua <ambrose@chua.family>
2019-07-06 12:16:50 +0000

    Add Inter font

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,123 @@
 <template>
-	<Toolbar />
+  <div>
+    <div
+      class="
+			head
+
+			bg-primary text-white
+			rounded-b-card
+			p-2"
+    >
+      <div class="m-4">
+        <h1
+          class="
+					font-bold
+					text-4xl
+				"
+        >
+          Training title
+        </h1>
+        <div
+          class="
+					font-bold
+				"
+        >
+          By Author
+        </div>
+      </div>
+      <div class="m-4" title="Major graph">
+        <trend
+          :data="major"
+          :gradient="gradient"
+          :height="200"
+          auto-draw
+          smooth
+        >
+        </trend>
+      </div>
+      <div
+        class="
+				flex items-stretch
+				m-4"
+      >
+        <Button
+          class="
+					flex-1
+					bg-white
+					hover:bg-white
+					text-primary"
+          >Pause</Button
+        >
+      </div>
+    </div>
+    <div class="p-2">
+      <div
+        class="
+				bg-white
+				rounded-card
+				shadow-md
+				m-4 p-4"
+      >
+        <h2
+          class="
+					font-medium
+					mx-2"
+        >
+          Runtime
+        </h2>
+        <div
+          class="
+					text-6xl font-bold
+					text-center"
+        >
+          10:55
+        </div>
+      </div>
+      <div
+        class="
+				bg-white
+				rounded-card
+				shadow-md
+				m-4 p-4"
+      >
+        <h2
+          class="
+					font-medium
+					mx-2"
+        >
+          Epoch time
+        </h2>
+        <trend :data="major" :height="200" auto-draw smooth> </trend>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style>
+.head {
+  background: linear-gradient(45deg, #ff6900, #ff994f);
+  padding-bottom: 8rem;
+  margin-bottom: -8rem;
+}
+</style>
+
 <script>
-import Toolbar from '~/components/client/Toolbar.vue'
+import Button from '~/components/common/Button.vue'
+import trend from 'vuetrend'
+
+const gradient = ['#ffffff', '#ff974d']
 
 export default {
   layout: 'client',
   components: {
-    Toolbar
+    Button,
+    trend
+  },
+  data() {
+    return {
+      gradient,
+      major: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]
+    }
   }
 }
 </script>
