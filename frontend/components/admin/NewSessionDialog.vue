1058d3b36722a030df794971956c5a34c90f1961
Ambrose Chua <ambrose@chua.family>
2019-07-06 23:03:19 +0000

    Blep

--- frontend/components/admin/NewSessionDialog.vue
+++ frontend/components/admin/NewSessionDialog.vue
@@ -1,32 +1,54 @@
 <template>
   <div class="fader fixed inset-0 p-6 flex items-center justify-center">
     <div class="bg-white rounded-card shadow-md m-6">
-      <div class="bg-primary text-white rounded-card flex items-center justify-start">
-		  <button class="m-5">
-			  <fa-icon size="2x" :icon="['far', 'times']" />
-		  </button>
-		<h1 class="text-2xl font-bold">New Session</h1>
+      <div
+        class="bg-primary text-white rounded-card flex items-center justify-start"
+      >
+        <button class="m-5">
+          <fa-icon size="2x" :icon="['far', 'times']" />
+        </button>
+        <h1 class="text-2xl font-bold flex-1">New Session</h1>
+        <button class="m-5 p-1" @click="submit">
+          Create
+        </button>
       </div>
-	  <div class="p-2">
-		  <div class="m-4">
-			  <input 
-			   class="text-2xl font-bold"
-				v-model="sessionName"
-				placeholder="Enter session name" />
-		  </div>
-		  <div class="m-4">
-			  <DropArea @input="sessionModel = $event" placeholder="Drop model here" />
-		  </div>
-		  <div class="m-4">
-			  <DropArea @input="sessionData = $event" placeholder="Drop data here" />
-		  </div>
-		  <div class="m-4">
-			  <DropArea @input="sessionLabels = $event" placeholder="Drop labels here" />
-		  </div>
-		  <div class="m-4">
-			  <DropArea @input="sessionDataParser = $event" placeholder="Drop data parser here" />
-		  </div>
-	  </div>
+      <div class="p-2">
+        <div class="m-4">
+          <input
+            class="text-2xl font-bold"
+            v-model="sessionName"
+            placeholder="Enter session name"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionModel = $event"
+            placeholder="Drop model here"
+            label="Model"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionData = $event"
+            placeholder="Drop data here"
+            label="Data"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionLabels = $event"
+            placeholder="Drop labels here"
+            label="Labels"
+          />
+        </div>
+        <div class="m-4">
+          <DropArea
+            @input="sessionDataParser = $event"
+            placeholder="Drop data parser here"
+            label="Data Parser"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -39,44 +61,65 @@
 
 <script>
 import DropArea from '~/components/common/DropArea.vue';
+import cuid from 'cuid';
 export default {
   components: {
   DropArea
   },
   data() {
     return {
-      sessionName: ''
+      sessionName: '',
+		sessionModel: null,
+	sessionData: null,
+		sessionLabels: null,
+		sessionDataParser: null,
+
     }
   },
   methods: {
-    onSubmit: async function(files, submitType) {
+	  async submit () {
       // let files = this.$refs.fileloader.files
 
-      var url = (function(type) {
-        switch (type) {
-          case 'dataset':
-            return 'localhost:10200/data'
-          case 'labels':
-            return 'localhost:10200/labels'
-          case 'scripts':
-            return 'localhost:10200/data_parse'
-          default:
-            return ''
-        }
-      })(submitType)
+		// validate
+		if (!this.sessionName || !this.sessionModel || !this.sessionData || !this.sessionLabels || !this.sessionDataParser) {
+			return;
+		}
 
-      for (var i = 0; i < files.length; i++) {
-        await fetch(url, {
-          method: 'PUT',
-          redirect: 'follow',
-          body: files[i]
-        })
+		// TODO: create meta.json
 
-        console.log(`Uploaded ${files[i].name}`)
-      }
+		const base = process.env.NUXT_ENV_BACKEND2_URL || 'http://localhost:10200';
+		  const modelid = cuid();
+		  const dataid = cuid();
+		  const dataparserid = cuid();
+		await fetch(base + '/model?id=' + modelid, {
+			method: 'PUT',
+			redirect: 'follow',
+			body: this.sessionModel,
+		});
+		await Promise.all([
+			async () => {
+				await fetch(base + '/data?model=' + modelid + '&id=' + dataid, {
+					method: 'PUT',
+					redirect: 'follow',
+					body: this.sessionData,
+				});
+			},
+			async () => {
+				await fetch(base + '/labels?model=' + modelid + '&id=' + dataid, {
+					method: 'PUT',
+					redirect: 'follow',
+					body: this.sessionLabels,
+				});
+			},
+			async () => {
+				await fetch(base + '/data_parser?model=' + modelid + '&id=' + dataparserid, {
+					method: 'PUT',
+					redirect: 'follow',
+					body: this.sessionDataParser,
+				});
+			},
+		]);
     }
   }
 }
 </script>
-
-<style></style>
--- frontend/components/common/DropArea.vue
+++ frontend/components/common/DropArea.vue
@@ -1,53 +1,66 @@
 <template>
-	<label class="bg-gray-100 text-gray-800 block text-center font-medium p-6" :class="{ 'text-gray-500': !filename, 'bg-gray-300': !filename }"
-		   @drop="dropInput"
-		   @dragover="dragOver"
-	:for="id">
-	{{ filename || placeholder }}
-	<input class="hidden" @input="clickInput"
-	type="file" :id="id" ref="file" />
-</label>
+  <label
+    class="block text-center font-medium p-6"
+    :class="{
+      'bg-gray-100': !!filename,
+      'text-gray-800': !!filename,
+      'text-gray-500': !filename,
+      'bg-gray-200': !filename
+    }"
+    @drop="dropInput"
+    @dragover="dragOver"
+    :for="id"
+  >
+    {{ filename ? label + ': ' + filename : placeholder }}
+    <input class="hidden" @input="clickInput" type="file" :id="id" ref="file" />
+  </label>
 </template>
 
 <script>
 export default {
-	props: {
-		placeholder: String,
-	},
-	data() {
-		return {
-			id: Math.floor(Math.random() * 10000000),
-			filename: '',
-		};
-	},
-	methods: {
-		dropInput(e) {
-			e.preventDefault();
-			try {
-				const item = e.dataTransfer.items[0];
-				if (!item) {
-					throw new Error("Item not found");
-				}
-				const file = item.getAsFile();
-				this.chosenOne(file);
-			} catch (e) {
-				this.filename = '';
-			}
-		},
-		dragOver(e) {
-			e.preventDefault();
-		},
-		clickInput(e) {
-			try {
-				const file = e.target.files;
-				this.chosenOne(file);
-			} catch (e) {
-				this.filename = '';
-			}
-		},
-		chosenOne(file) {
-			console.log(file);
-		},
-	},
+  props: {
+    label: String,
+    placeholder: String
+  },
+  data() {
+    return {
+      id: Math.floor(Math.random() * 10000000),
+      filename: ''
+    }
+  },
+  methods: {
+    dropInput(e) {
+      e.preventDefault()
+      try {
+        const item = e.dataTransfer.items[0]
+        if (!item) {
+          throw new Error('Item not found')
+        }
+        const file = item.getAsFile()
+        this.chosenOne(file)
+      } catch (e) {
+        this.filename = ''
+      }
+    },
+    dragOver(e) {
+      e.preventDefault()
+    },
+    clickInput(e) {
+      try {
+        const file = e.target.files[0]
+        if (!file) {
+          throw new Error('Item not found')
+        }
+        this.chosenOne(file)
+      } catch (e) {
+        this.filename = ''
+      }
+    },
+    chosenOne(file) {
+      this.filename = file.name
+      console.log(file)
+      this.$emit('input', file)
+    }
+  }
 }
 </script>
