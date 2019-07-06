a237de79eb4fa7fedfb59a6a9cca544303112ba8
UnicodingUnicorn <7555ic@gmail.com>
2019-07-06 21:05:47 +0800

    mnist data parsing

--- mnist_data_parser.lua
+++ mnist_data_parser.lua
@@ -5,14 +5,14 @@ image_size = 0
 current = ""
 
 function to_i32 (b1, b2, b3, b4)
-  local val = ((b1 & 0xFF) << 0) | ((b2 & 0xFF) <<  8) | ((b3 & 0xFF) << 16) | ((b4 & 0xFF) << 24)
+  local val = b4 + b3 * 8 + b2 * 16 + b1 * 24;
   return val
 end
 
 function parse (chunk, n) -- chunk is string of bytes
   if not parsed_headers then
-    local row_size = to_i32(string.byte(chunk, 8), string.byte(chunk, 9), string.byte(chunk, 10), string.byte(chunk, 11))
-    local col_size = to_i32(string.byte(chunk, 12), string.byte(chunk, 13), string.byte(chunk, 14), string.byte(chunk, 15))
+    local row_size = to_i32(string.byte(chunk, 9), string.byte(chunk, 10), string.byte(chunk, 11), string.byte(chunk, 12))
+    local col_size = to_i32(string.byte(chunk, 13), string.byte(chunk, 14), string.byte(chunk, 15), string.byte(chunk, 16))
     image_size = row_size * col_size
     parsed_headers = true
     position = 17
